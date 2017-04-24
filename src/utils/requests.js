90d65c3fe52994cf9766e4e6125540c3ff9c2d27
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-24 12:09:06 -0700

    Add helper file for building Axios requests

---
+++ src/utils/requests.js
@@ -0,0 +1,36 @@
+const defaultRequest = (method, url) => ({
+  method,
+  headers: {
+    'Accept': 'application/json'
+  },
+  url
+})
+
+export default {
+  /**
+   * Builds a GET AJAX request formatted for the Axios library.
+   * Mostly just a shortcut to save writing everything out over and over
+   */
+  axGet (url, authToken = null) {
+    let req = defaultRequest('get', url)
+
+    if (authToken) {
+      req.headers['Authorization'] = `Token ${authToken}`
+    }
+
+    return req
+  },
+
+  /**
+   * Builds a POST AJAX request formatted for the Axios library.
+   */
+  axPost (url, data, token = null) {
+    let req = Object.assign({}, defaultRequest('post', url), { data })
+
+    if (token) {
+      req.headers['Authorization'] = `Token ${token}`
+    }
+
+    return req
+  }
+}
