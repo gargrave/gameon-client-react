c27178b6540c27a8b4a005ce8a0b7171a8b34c3b
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-24 16:01:18 -0700

    Add-predefined errors file

---
+++ src/globals/errors.js
@@ -0,0 +1,9 @@
+/* ============================================
+ = Validation Errors
+ ============================================= */
+
+export const valErrs = {
+  required: 'This field is required.',
+  email: 'Must be a valid email address.',
+  length: (min) => `Must be at least ${min} characters long.`
+}
