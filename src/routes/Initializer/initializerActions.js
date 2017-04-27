ba51abd8cc66ee968db7351e2c7a52a31e78ea55
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-27 12:20:58 -0700

    Remove unnecessary imports

--- src/routes/Initializer/initializerActions.js
+++ src/routes/Initializer/initializerActions.js
@@ -1,8 +1,3 @@
-import axios from 'axios'
-
-import { apiUrls } from '../../globals/urls'
-import requests from '../../utils/requests'
-
 export const types = {
   INITIALIZER_BEGIN: 'INITIALIZER_BEGIN',
   INITIALIZER_END: 'INITIALIZER_END'
