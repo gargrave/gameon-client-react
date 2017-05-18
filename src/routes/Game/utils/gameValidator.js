fb9c7c80cc957b25d9e0ad4d1d63bd78d80923dc
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-05-18 15:20:16 -0700

    Update Game comparison methodology

--- src/routes/Game/utils/gameValidator.js
+++ src/routes/Game/utils/gameValidator.js
@@ -2,6 +2,17 @@ import validator from 'validator'
 
 import { valErrs } from '../../../globals/errors'
 
+function parsePlatform (game) {
+  if (game.platform) {
+    if (Number.isInteger(game.platform)) {
+      return game.platform
+    } else if (game.platform.id) {
+      return game.platform.id
+    }
+  }
+  throw new TypeError('Game does not have valid Platform data.')
+}
+
 export function validate (data) {
   let valid = true
   let testData = Object.assign({}, data)
@@ -21,11 +32,27 @@ export function validate (data) {
 export function compare (a, b) {
   let match = true
 
+  // compare 'id' prop
   if ((a.id && b.id) && (Number(a.id) !== Number(b.id))) {
     match = false
   }
 
+  // compare 'title' prop
   if ((a.title.trim() !== b.title.trim())) {
+    match = false
+  }
+
+  // compare 'finished' prop
+  if ((a.finished !== b.finished)) {
+    match = false
+  }
+
+  // compare 'platform' prop
+  // need to make sure we are only comparing platform ids,
+  // and not the full platform object
+  const platformA = parsePlatform(a)
+  const platformB = parsePlatform(b)
+  if (platformA !== platformB) {
     match = false
   }
 
