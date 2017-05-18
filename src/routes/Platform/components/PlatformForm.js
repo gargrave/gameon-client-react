ea8897a9c54ac29308c709de8038c46a4f24c766
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-05-17 17:11:45 -0700

    Remove Semantic from PlatformForm

--- src/routes/Platform/components/PlatformForm.js
+++ src/routes/Platform/components/PlatformForm.js
@@ -1,59 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, Dimmer, Form, Loader } from 'semantic-ui-react'
+
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 const PlatformForm = (props) => (
   <div>
-    <Form id='platform-form'>
+    <form id='platform-form'>
 
-      <Dimmer inverted active={props.working}>
-        <Loader inverted>Working...</Loader>
-      </Dimmer>
+      {props.working && <p>Working...</p>}
 
-      <Form.Field required
-        error={!!props.errors.title}>
-        <label htmlFor='title'>Title</label>
-        {!!props.errors.title &&
-          <p className='form-error' id='platform-title-error'>{props.errors.title}</p>
-        }
-        <input
-          type='text'
-          id='platform-title'
-          name='title'
-          placeholder='title'
-          value={props.platformData.title}
-          onChange={props.onChange}
-        />
-      </Form.Field>
+      <TextField
+        fullWidth
+        required
+        id='platform-input-title'
+        name='title'
+        hintText='Title'
+        floatingLabelText='Title'
+        errorText={props.errors.title}
+        value={props.platformData.title}
+        onChange={props.onChange}
+      />
 
       <hr />
 
-      <Button
+      <RaisedButton
         primary
-        id='platform-submit'
+        id='platform-btn-submit'
+        className='go-btn'
         type='submit'
+        label='Submit'
         disabled={props.disabled}
-        onClick={props.onSubmit}>
-        Submit
-      </Button>
+        onClick={props.onSubmit}
+      />
 
-      <Button
-        id='platform-cancel'
-        onClick={props.onCancel}>
-        Cancel
-      </Button>
+      <RaisedButton
+        id='platform-btn-cancel'
+        className='go-btn'
+        label='Cancel'
+        onClick={props.onCancel}
+      />
 
       {props.onDelete &&
-        <Button
-          negative
-          id='platform-delete'
-          floated='right'
-          onClick={props.onDelete}>
-          Delete
-        </Button>
+        <RaisedButton
+          secondary
+          id='platform-btn-delete'
+          className='go-btn'
+          label='Delete'
+          onClick={props.onDelete}
+        />
       }
-
-    </Form>
+    </form>
   </div>
 )
 
